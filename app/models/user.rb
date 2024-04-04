7b4f8c73f7cdcab024a34e2dab47c0192ff6b22f
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2024-04-04 02:09:38 +0000

    [Finish]いいね/コメント機能」を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
