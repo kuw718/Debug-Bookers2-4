cae0ca8f9be902158556c2a245904f04469c51c9
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2024-04-08 02:16:43 +0000

    [Finish]課題3で実装した機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    #redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    #redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 end
